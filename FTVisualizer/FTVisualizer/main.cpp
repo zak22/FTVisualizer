48678be2601b51a4598b2e2305496d198f255a8b
Zakaria Almatar <zeeekoo@gmail.com>
2011-07-10 02:23:13 -0700

    added option to specify width in func rectangle

--- FTVisualizer/FTVisualizer/main.cpp
+++ FTVisualizer/FTVisualizer/main.cpp
@@ -17,45 +17,9 @@ float blue[]={0,0,153}, pink[]={204,0,153}, white[]={1,1,1};
 
 
 
-void rectangle(float xLeft, float xRight, float yBottom, float yTop, float *color);
-
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	rectangle(-1,1,-0.5,0.5, white);
-
-	glFlush();
-    glutPostRedisplay();
-}
-
-
-void init()
-{
-	glClearColor(0,0,0,0);
-	gluOrtho2D(-5,5,-5,5);
-}
-
-
-void rectangle(float xLeft,float xRight,float yBottom,float yTop, float *color)
-{
-    glColor3f(color[0],color[1],color[2]);
-
-	glBegin(GL_LINES);
-
-	glVertex2f(xLeft,yTop);
-	glVertex2f(xLeft,yBottom);
-
-    glVertex2f(xRight,yTop);
-	glVertex2f(xRight,yBottom);
-
-    glVertex2f(xLeft,yTop);
-	glVertex2f(xRight,yTop);
-
-    glVertex2f(xLeft,yBottom);
-	glVertex2f(xRight,yBottom);
-
-    glEnd();
-}
+void rectangle(float xLeft, float xRight, float yBottom, float yTop,float width, float *color);
+void display();
+void init();
 
 
 void main(int argc, char ** argv)
@@ -69,5 +33,60 @@ void main(int argc, char ** argv)
 	glutDisplayFunc(display);
     glutMainLoop();
 }
+
+
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	rectangle(-3,-1,-0.5,0.5,0.1, pink);
+	rectangle(1,3,-0.5,0.5,0.1, blue);
+	
+	glFlush();
+    glutPostRedisplay();
+}
+
+
+void init()
+{
+	glClearColor(0,0,0,0);
+	gluOrtho2D(-5,5,-5,5);
+}
+
+
+void rectangle(float xLeft,float xRight,float yBottom,float yTop,float width, float *color)
+{
+    glColor3f(color[0],color[1],color[2]);
+
+	glBegin(GL_QUADS);
+
+	glVertex2f(xLeft,yTop);
+	glVertex2f(xLeft-width,yTop);
+	glVertex2f(xLeft-width,yBottom);
+	glVertex2f(xLeft,yBottom);
+
+
+	glVertex2f(xRight,yTop);
+	glVertex2f(xRight+width,yTop);
+	glVertex2f(xRight+width,yBottom);
+	glVertex2f(xRight,yBottom);
+
+    
+	glVertex2f(xLeft,yTop);
+	glVertex2f(xLeft,yTop+width);
+	glVertex2f(xRight,yTop+width);
+	glVertex2f(xRight,yTop);
+
+
+	glVertex2f(xLeft,yBottom);
+    glVertex2f(xLeft,yBottom-width);
+	glVertex2f(xRight,yBottom-width);
+	glVertex2f(xRight,yBottom);
+
+    glEnd();
+}
+
+
 
 
